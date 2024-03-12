1210aacff3b395c21cbaa3b86bbed6bcddeaa9b3
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-03-13 00:46:20 +0700

    chore: updating main container background color

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Calculator from "./components/calculator";
 
 export default function App() {
   return (
-    <main className="w-full min-h-screen flex flex-col gap-2 items-center justify-center font-mono">
+    <main className="w-full min-h-screen flex flex-col gap-2 items-center justify-center font-mono bg-stone-900">
       <Calculator />
     </main>
   );
