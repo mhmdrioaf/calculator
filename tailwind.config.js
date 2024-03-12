9c26de9b839ef106215861e2dfbcb28876197ba5
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-03-13 00:32:02 +0700

    chore: adding custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "rgb(var(--primary) / <alpha-value>)",
+        "primary-foreground": "rgb(var(--primary-foreground) / <alpha-value>)",
+        secondary: "rgb(var(--secondary) / <alpha-value>)",
+        "secondary-foreground":
+          "rgb(var(--secondary-foreground) / <alpha-value>)",
+        destructive: "rgb(var(--destructive) / <alpha-value>)",
+        "destructive-foreground":
+          "rgb(var(--destructive-foreground) / <alpha-value>)",
+        tertiary: "rgb(var(--tertiary) / <alpha-value>)",
+        "tertiary-foreground":
+          "rgb(var(--tertiary-foreground) / <alpha-value>)",
+      },
+    },
   },
   plugins: [],
 };
